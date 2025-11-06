875d2e3790d59d247415a1f83f5cf366f01ed3f7
Oliverngu <138084319+Oliverngu@users.noreply.github.com>
2025-11-06 21:29:21 +0100

    Update FoglalasokApp.tsx

--- src/ui/components/apps/FoglalasokApp.tsx
+++ src/ui/components/apps/FoglalasokApp.tsx
@@ -3,12 +3,12 @@ import { Booking, User, Unit } from '../../data/mockData';
 import { db, Timestamp, serverTimestamp } from '../../firebase/config';
 import { collection, query, where, orderBy, onSnapshot, addDoc, doc, updateDoc } from 'firebase/firestore';
 import BookingIcon from '../src/ui/components/icons/BookingIcon';
-import LoadingSpinner from '..src/LoadingSpinner';
-import AddBookingModal from '..src/ui/components/AddBookingModal';
-import PlusIcon from '..src/ui/components/icons/PlusIcon';
-import SettingsIcon from '..src/ui/components/icons/SettingsIcon';
-import ReservationSettingsModal from '.src/ui/components/ReservationSettingsModal';
-import TrashIcon from '..src/ui/components/icons/TrashIcon';
+import LoadingSpinner from '../src/LoadingSpinner';
+import AddBookingModal from '../src/ui/components/AddBookingModal';
+import PlusIcon from '../src/ui/components/icons/PlusIcon';
+import SettingsIcon from '../src/ui/components/icons/SettingsIcon';
+import ReservationSettingsModal from '../src/ui/components/ReservationSettingsModal';
+import TrashIcon from '../src/ui/components/icons/TrashIcon';
 
 interface FoglalasokAppProps {
   currentUser: User;
